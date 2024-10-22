fbc7b6a75dfbb526e51cf65da59b89b3820e8f13
tylermcgraw <=>
2024-10-21 21:06:21 -0400

    context loss exercises

---
+++ lesson-2/context-loss.js
@@ -0,0 +1,91 @@
+/*
+1. undefined undefined is a undefined
+   This is because func() on line 12 calls getDescription in the global context
+
+2. below
+*/
+
+let turk = {
+  firstName: 'Christopher',
+  lastName: 'Turk',
+  occupation: 'Surgeon',
+  getDescription() {
+    return this.firstName + ' ' + this.lastName + ' is a '
+                                + this.occupation + '.';
+  }
+};
+
+function logReturnVal(func, context) {
+  let returnVal = func.call(context);
+  console.log(returnVal);
+}
+
+logReturnVal(turk.getDescription, turk);
+
+/*
+3. let turkDescription = turk.getDescription.bind(turk);
+
+4. No, it will output undefined: Arena, etc
+   This is because the callback function inside forEach loses its context
+   and so this.seriesTitle refers to the global object
+
+5. below
+*/
+
+const TESgames = {
+  titles: ['Arena', 'Daggerfall', 'Morrowind', 'Oblivion', 'Skyrim'],
+  seriesTitle: 'The Elder Scrolls',
+  listGames: function() {
+    let self = this;
+    this.titles.forEach(function(title) {
+      console.log(self.seriesTitle + ': ' + title);
+    });
+  }
+};
+
+TESgames.listGames();
+
+/*
+6. below
+const TESgames = {
+  titles: ['Arena', 'Daggerfall', 'Morrowind', 'Oblivion', 'Skyrim'],
+  seriesTitle: 'The Elder Scrolls',
+  listGames: function() {
+    this.titles.forEach(function(title) {
+      console.log(this.seriesTitle + ': ' + title);
+    }, this);
+  }
+};
+
+TESgames.listGames();
+
+7. below
+const TESgames = {
+  titles: ['Arena', 'Daggerfall', 'Morrowind', 'Oblivion', 'Skyrim'],
+  seriesTitle: 'The Elder Scrolls',
+  listGames: function() {
+    this.titles.forEach(title => {
+      console.log(this.seriesTitle + ': ' + title);
+    });
+  }
+};
+
+8. 0
+
+9. below
+*/
+
+let foo = {
+  a: 0,
+  incrementA: function() {
+    function increment() {
+      this.a += 1;
+    }
+
+    increment.call(this);
+  }
+};
+
+foo.incrementA();
+foo.incrementA();
+foo.incrementA();
