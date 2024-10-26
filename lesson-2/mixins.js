e2644cde7cdf2adc45812ade1fd2ee7f29b62f80
tylermcgraw <=>
2024-10-26 08:15:40 -0400

    mixins exercises

---
+++ lesson-2/mixins.js
@@ -0,0 +1,72 @@
+const Speed = {
+  goFast() {
+    console.log(`I'm a ${this.constructor.name} and going super fast!`);
+  }
+};
+
+class Car {
+  goSlow() {
+    console.log(`I'm safe and driving slow.`);
+  }
+}
+Object.assign(Car.prototype, Speed);
+
+class Truck {
+  goVerySlow() {
+    console.log(`I'm a heavy truck and like going very slow.`);
+  }
+}
+Object.assign(Truck.prototype, Speed);
+
+let car = new Car();
+let truck = new Truck();
+car.goFast();
+truck.goFast();
+
+const Vehicle = {
+  range() {
+    return this.fuelCap *  this.fuelEfficiency;
+  }
+};
+
+class WheeledVehicle {
+  constructor(tirePressure, kmTravelledPerLiter, fuelCapInLiter) {
+    this.tires = tirePressure;
+    this.fuelEfficiency = kmTravelledPerLiter;
+    this.fuelCap = fuelCapInLiter;
+  }
+
+  tirePressure(tireIdx) {
+    return this.tires[tireIdx];
+  }
+
+  inflateTire(tireIdx, pressure) {
+    this.tires[tireIdx] = pressure;
+  }
+}
+Object.assign(WheeledVehicle.prototype, Vehicle);
+
+class Auto extends WheeledVehicle {
+  constructor() {
+    // the array represents tire pressure for four tires
+    super([30,30,32,32], 50, 25.0);
+  }
+}
+
+class Motorcycle extends WheeledVehicle {
+  constructor() {
+    // array represents tire pressure for two tires
+    super([20,20], 80, 8.0);
+  }
+}
+
+class Catamaran {
+  constructor(propellerCount, hullCount, kmTravelledPerLiter, fuelCapInLiter) {
+    // catamaran specific logic
+    this.propellerCount = propellerCount;
+    this.hullCount = hullCount;
+    this.fuelEfficiency = kmTravelledPerLiter;
+    this.fuelCap = fuelCapInLiter;
+  }
+}
+Object.assign(Catamaran.prototype, Vehicle);
